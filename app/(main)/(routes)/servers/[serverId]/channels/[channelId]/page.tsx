c55dece9c0666305b1c8af63eb149e0ae55778ba
lhikarul <711hikaru@gmail.com>
2024-05-04 17:31:31 +0800

    feat: chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,48 @@
-const ChannelIdPage = () => {
-  return <div>Channel Id Page</div>;
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProsp {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+const ChannelIdPage = async ({ params }: ChannelIdPageProsp) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.discordChannel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.discordMember.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
